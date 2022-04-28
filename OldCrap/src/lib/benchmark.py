3171e084d28b1ad1c638d95fffe4badeb88f3ac7
Cryowatt <Cryowatt@users.noreply.github.com>
2022-04-28 23:09:40 +0000

    Git' out of here

--- OldCrap/src/lib/benchmark.py
+++
@@ -1,6 +0,0 @@
-import time
-
-def benchmark(function):
-    start = time.ticks_us()
-    function()
-    return time.ticks_diff(time.ticks_us(), start)
--- OldCrap/src/pylib/benchmark.py
+++
@@ -1,6 +0,0 @@
-import time
-
-def benchmark(function):
-    start = time.perf_counter_ns()
-    function()
-    return time.perf_counter_ns() - start
